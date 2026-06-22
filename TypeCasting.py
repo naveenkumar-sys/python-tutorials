c3997e84f728e249c529125fe7dec9c05d82f5de
naveenkumar-sys <naveenrnk69@gmail.com>
2026-06-22 10:12:07 +0530

    input and type casting updated

--- TypeCasting.py
+++ TypeCasting.py
@@ -1,10 +1,46 @@
 #type casting => convert one data type to another
 #int() , float() , str()
+
 # Note: we cant change the natural type like we cannot change string to integer
+# important note 
+
+# Type casting works only if the value can actually be converted to that type.
+# str()   → Almost anything can become a string ✅
+
+# int()   → Must look like an integer ✅
+
+# float() → Must look like a number ✅
+
+
+# # More Examples:
+    
+# String → Integer
+# int("50")     # ✅ 50
+# int("-50")    # ✅ -50
+# int("5.5")    # ❌ Error
+# int("abc")    # ❌ Error
+
+# String → Float
+# float("5")      # ✅ 5.0
+# float("5.5")    # ✅ 5.5
+# float("abc")    # ❌ Error
+
+# Integer → String
+# str(123)      # ✅ "123"
 
 #There are two types of type casting 
-#1. Implicit type casting - by default with pyhton interpretor
+#1. Implicit type casting - by default with python interpretor
 #2. Explicit type casting - by user with inbuilt function
+# 1.Implicit type casting 
+a = 10      # int
+b = 2.5     # float
+
+result = a + b
+
+print(result)
+print(type(result))
+
+#2.explicit type casting 
 name ="name"
 print(type(name))
 print(int(name)) # ValueError: invalid literal for int() with base 10: 'name'
--- input.py
+++ input.py
@@ -16,14 +16,51 @@
 # full_name = " ".join(sys.argv[1:]) #terminal act as array so we take input  1 st index and above  and join is convert array to string in js we arr.join(" ")
 
 # print(full_name)
+    
+    
+# #check the user input is string od number
+# #checking type of the input -> note that the user input method always return string  
+# num1=input("Enter a value:")
+# print(type(num1))
+
+# #function that convert input string as int and float 
+# def check_user_input(input_val):
+#     try:
+#         val=int(input_val)
+#         print(f"The given Input {val} is Interger")
+#     except ValueError:
+#         try:
+#             val=float(input_val)
+#             print(f"The give input {val} is float")
+#         except ValueError:
+#             print("The given value is String")
+        
+# input_val=input("Enter the value:")
+# check_user_input(input_val)
 
 
-# #here input is always right 
-# name = input("enter your name:")
-# print(type(name))
 
-num5 =int(input("Enter a number1:"))
-num6=float(input("Enter number2:"))
+# #checking the  number using string method called isdigit()
+# val=input("Enter a number:")
+# if val.isdigit():
+#     print("The value is number")
+# else:
+#     print("The given value is string")
+    
+#Only accept a number as input - when the user type number related and float related will be accept and stop the loop and otherwise it will run 
+while True:
+    input_val=input("Enter the value:")
+    try:
+        val=int(input_val)
+        print(f"The given Input {val} is Interger")
+        break;
+    except ValueError:
+        try:
+            val=float(input_val)
+            print(f"The give input {val} is float")
+            break;
+        except ValueError:
+            print("The given value is String")
+        
 
-if num5 and num6:
-    print(num5*num6)
+
