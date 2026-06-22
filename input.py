a9f79f25c6fc5c845b870cdd981323ffb47d32f4
naveenkumar-sys <naveenrnk69@gmail.com>
2026-06-22 12:18:23 +0530

    input.py updated

--- input.py
+++ input.py
@@ -47,20 +47,92 @@
 # else:
 #     print("The given value is string")
     
-#Only accept a number as input - when the user type number related and float related will be accept and stop the loop and otherwise it will run 
-while True:
-    input_val=input("Enter the value:")
-    try:
-        val=int(input_val)
-        print(f"The given Input {val} is Interger")
-        break;
-    except ValueError:
-        try:
-            val=float(input_val)
-            print(f"The give input {val} is float")
-            break;
-        except ValueError:
-            print("The given value is String")
-        
+# #Only accept a number as input - when the user type number related and float related will be accept and stop the loop and otherwise it will run 
+# while True:
+#     input_val=input("Enter the value:")
+#     try:
+#         val=int(input_val)
+#         print(f"The given Input {val} is Interger")
+#         break;
+#     except ValueError:
+#         try:
+#             val=float(input_val)
+#             print(f"The give input {val} is float")
+#             break;
+#         except ValueError:
+#             print("The given value is String")
+
+# #Python take a list as input from a user
+# # Using the Python input() function, we can directly accept strings, numbers, and characters from the user.
+# # However, to take a list as input from a user, we need to perform some parsing on user input to convert it into a list        
+
+# # How to Get a list of numbers as input from a user in Python
+
+# #Example: Get a list of numbers as input from a user and calculate the sum of it
+# values=input("Enter a list of numbers with:").split(" ")
+# new_list=[]
+# # print(values)
+# for val in values:
+#     new_value=int(val)
+#     new_list.append(new_value)
+
+# print(new_list)
+# print(type(new_list))
 
 
+# #Example:Input a list using input() and range() function in Python
+# # number_list=[]
+# # size=int(input("Enter the size of the list:"))
+
+# # for i in range(0,size):
+# #     val=int(input(f"Enter index {i} value:"))
+# #     number_list.append(val)
+# # print(number_list)
+
+# #Example:Input a list using a list comprehension in Python
+# size=int(input("Enter a size of thwe list:"))
+
+# # new_list=list((2,3,4,5,6,7))
+# # print(new_list)
+
+# new_list=list(int(num) for num in input("Enter the value:").strip().split(" "))[:size] #We are running for loop to get user input and remove the white space , and split into array, now each value beace number and pass the number in the list() method to convert that into list again
+# print(new_list)
+
+# #Example:Input a list using the map function
+# n=int(input("Enter the size of List:"))
+
+# new_list=list(map(float,input("Enter a number:").strip().split()))[:n]
+# print(new_list)
+
+# #Example:Get a list of strings as input from a user
+# family_name=input("Enter family member name follwed with witeSpace:").strip().split(" ")
+# # family_name=list((input("Enter a name:"))) it will split into "n" "a" "v" "e" "e" "n"
+# # print(family_name)
+
+# for name in family_name:
+#     print(name)
+
+# #Example:Accept a nested list as input
+# outer=[]
+# size=int(input("Enter the size:"))
+
+# for i in range(0,size):
+#     new_list=list(map(int,input("Enter a List of number:").strip().split()))
+#     outer.append(new_list)
+    
+# print(outer)
+
+#Enter the inner list input line by line
+outer=[]
+outer_size=int(input("Enter the Outer list size:"))
+inner_size=int(input("Enter inner list size:"))
+
+for i in range(0,outer_size):
+    inner=[]
+    for j in range(0,inner_size):
+        val=int(input(f"Enter {i} list of {j} value :"))
+        inner.append(val)
+    outer.append(inner)
+    print("--------------")
+print(outer)
+    
