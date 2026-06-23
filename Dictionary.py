daf6217daed570233cec553e8542e1e195dc0452
naveenkumar-sys <naveenrnk69@gmail.com>
2026-06-23 18:27:03 +0530

    dictionary updated

---
+++ Dictionary.py
@@ -0,0 +1,18 @@
+# Dictionaries are ordered collections of unique values stored in (Key-Value) pairs.
+
+#3 ways to create 
+#1.By enclosing elements in the curly brackets {}
+new_list={
+    "name":"Baleno",
+    "Brand": "Suzuki",
+    "Model":2021,
+    "Price":1000000
+}
+
+# print(new_list)
+# for key ,value in new_list.items():
+#     print(key,value)
+#2.By using dict() class
+new1_list=(dict({"naveen":20,"age":21,"marks":80}))
+print(new1_list)
+#3.By using a dictionary literal
